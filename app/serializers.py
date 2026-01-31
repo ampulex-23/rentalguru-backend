4f15a2e6a2a4ca64d4432ce4152866e02a7c0579
ampulex-23 <frwalkr@gmail.com>
2026-01-31 20:20:14 +0300

    fix: add id field to UserDetailSerializer for mobile app wallet

--- app/serializers.py
+++ app/serializers.py
@@ -241,6 +241,7 @@ class UserDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
+            'id',
             'first_name',
             'last_name',
             'email',
